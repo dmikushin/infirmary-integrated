1ab3c140eadde847d4dba29efde1cd166775ff0f
Dmitry Mikushin <dmitry@kernelgen.org>
2020-03-17 11:19:30 +0100

    Restructuring project, starting CMake build system

---
+++ cmake/UseMono.cmake
@@ -0,0 +1,14 @@
+#
+# Adopted from https://github.com/vgvassilev/solid
+#
+# A CMake Module for using Mono.
+#
+# The following variables are set:
+#   (none)
+#
+# Additional references can be found here:
+#   http://www.mono-project.com/Main_Page
+#   http://www.mono-project.com/CSharp_Compiler
+#
+
+message( STATUS "Using Mono compiler version ${CSHARP_MONO_VERSION}" )
